77be62d25277355cd0202168658d258cd08e7852
sh970901 <aaddss639@naver.com>
2022-08-22 11:37:52 +0900

    sbb 시큐리티 로그인, 게시판, 답변

---
+++ src/main/java/com/ll/exam/sbb/user/SiteUser.java
@@ -0,0 +1,30 @@
+package com.ll.exam.sbb.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class SiteUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    public SiteUser(long id) {
+        this.id = id;
+    }
+}
---
+++ src/test/java/com/ll/exam/sbb/UserServiceTests.java
@@ -0,0 +1,62 @@
+package com.ll.exam.sbb;
+
+import com.ll.exam.sbb.answer.AnswerRepository;
+import com.ll.exam.sbb.question.QuestionRepository;
+import com.ll.exam.sbb.user.UserRepository;
+import com.ll.exam.sbb.user.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceTests {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        clearData();
+        createSampleData();
+    }
+
+    public static void createSampleData(UserService userService) {
+        userService.create("admin", "admin@test.com", "1234");
+        userService.create("user1", "user1@test.com", "1234");
+    }
+
+    private void createSampleData() {
+        createSampleData(userService);
+    }
+
+    public static void clearData(UserRepository userRepository, AnswerRepository answerRepository, QuestionRepository questionRepository) {
+        answerRepository.deleteAll();
+        answerRepository.truncateTable();
+
+        questionRepository.deleteAll();
+        questionRepository.truncateTable();
+
+        userRepository.deleteAll();
+        userRepository.truncateTable();
+    }
+
+    private void clearData() {
+        clearData(userRepository, answerRepository, questionRepository);
+    }
+
+    @Test
+    @DisplayName("회원가입이 가능하다.")
+    public void t1() {
+        userService.create("user2", "user2@email.com", "1234");
+    }
+}
