a47ea96806af1f9cbb52f6f1b20abbbb1c56507c
sh970901 <aaddss639@naver.com>
2022-08-23 14:49:02 +0900

    앵커효과 추가

--- src/main/java/com/ll/exam/sbb/answer/AnswerService.java
+++ src/main/java/com/ll/exam/sbb/answer/AnswerService.java
@@ -9,12 +9,13 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 
 
+
 @Service
 @RequiredArgsConstructor
 public class AnswerService {
     private final AnswerRepository answerRepository;
 
-    public void create(Question question, String content, SiteUser author) {
+    public Answer create(Question question, String content, SiteUser author) {
         Answer answer = new Answer();
         answer.setContent(content);
         answer.setCreateDate(LocalDateTime.now());
@@ -22,6 +23,8 @@ public class AnswerService {
         question.addAnswer(answer);
 
         answerRepository.save(answer);
+
+        return answer;
     }
 
     public Answer getAnswer(Long id) {
