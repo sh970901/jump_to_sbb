4e98f39e40a2a209bbcce94c216a501a255076d4
sh970901 <aaddss639@naver.com>
2022-08-24 14:36:26 +0900

    파라미터로 질문제목+질문내용으로 검색

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -33,8 +33,9 @@ public class QuestionController {
 
     @GetMapping("/list")
     // 이 자리에 @ResponseBody가 없으면 resources/question_list/question_list.html 파일을 뷰로 삼는다.
-    public String list(Model model, @RequestParam(defaultValue = "0") int page) {
-        Page<Question> paging = questionService.getList(page);
+    public String list(@RequestParam(required = false) String kw, @RequestParam(required = false) String username, Model model, @RequestParam(defaultValue = "0") int page) {
+
+        Page<Question> paging = questionService.getList(kw, page);
 
         // 미래에 실행된 question_list.html 에서
         // questionList 라는 이름으로 questionList 변수를 사용할 수 있다.
@@ -125,4 +126,10 @@ public class QuestionController {
         questionService.vote(question, siteUser);
         return "redirect:/question/detail/%d".formatted(id);
     }
+//    @GetMapping("/list")
+//    @ResponseBody
+//    public void listBoard(@PathVariable String title){
+//        System.out.println(title);
+//
+//    }
 }
